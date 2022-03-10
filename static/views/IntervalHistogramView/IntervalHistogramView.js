da181a36f0c243c3efc08ae0e26cd5b10f6671d0
sayefsakin <sayefsakin@yahoo.co.in>
2022-03-10 12:13:58 -0700

    dependency tree expand and sorted interval histogram primitive

--- static/views/IntervalHistogramView/IntervalHistogramView.js
+++ static/views/IntervalHistogramView/IntervalHistogramView.js
@@ -146,7 +146,7 @@ class IntervalHistogramView extends
       // Still loading...
       return;
     }
-    primitiveList = ['all_primitives', null].concat(Object.keys(primitiveList));
+    primitiveList = ['all_primitives', null].concat(Object.keys(primitiveList).sort());
     let options = this.primitiveMenu.selectAll('option')
       .data(primitiveList, d => d);
     options.exit().remove();
