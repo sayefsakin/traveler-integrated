4dee1c30e5d885e3def64a89a88d924aff721b5c
Sayef Azad Sakin <sayefsakin@yahoo.co.in>
2025-05-13 01:50:53 -0600

    Updated agg cluster binned query

--- data_handler/cgal_libs/cgal_server/agglomerate_clustering.cpp
+++ data_handler/cgal_libs/cgal_server/agglomerate_clustering.cpp
@@ -43,7 +43,33 @@ void EventAgglomerateClustering::buildAggCluster() {
     merge = new int[2*(npoints-1)];
     height = new double[npoints-1];
     node_size = new int[2*(npoints-1)];
+    start_events = new double[2*(npoints-1)];
+    end_events = new double[2*(npoints-1)];
     hclust_fast(npoints, distmat, opt_method, merge, height, node_size);
+
+    //
+    int left_node_index, right_node_index, root_node_index;
+    for (i=0; i<npoints-1; i++) {
+      left_node_index = changeMerge(merge[i], npoints);
+      if(left_node_index < npoints) {
+        start_events[left_node_index] = data[left_node_index*2];
+        end_events[left_node_index] = data[(left_node_index*2)+1];
+      }
+
+      right_node_index = changeMerge(merge[i+npoints-1], npoints);
+      if(right_node_index < npoints) {
+        start_events[right_node_index] = data[right_node_index*2];
+        end_events[right_node_index] = data[(right_node_index*2)+1];
+      }
+
+      if(end_events[left_node_index] > start_events[right_node_index]) {
+        std::swap(left_node_index, right_node_index);
+      }
+      
+      root_node_index = i + npoints;
+      start_events[root_node_index] = start_events[left_node_index];
+      end_events[root_node_index] = end_events[right_node_index];
+    }
     
     delete[] distmat;
     PRINTLOG("Building Agglomerate Clusters Done!");
@@ -71,56 +97,93 @@ int EventAgglomerateClustering::searchEvent(int64_t tb, int begin_index) {
   return s_begin;
 }
 
+// search logic
+// if bin size is less than cluster length, then go down
+// else return the start end point of the current cluster
+// dfs on the start and end time query
+void EventAgglomerateClustering::findClusters(int64_t start_t, int64_t end_t, int64_t bin_size, int c_node, std::vector<int64_t> &results) {
+  int root_node = c_node + npoints;
+  int64_t start_time = (int64_t)start_events[root_node];
+  int64_t end_time = (int64_t)end_events[root_node];
+  if(start_time >= end_t || end_time <= start_t) return;
+  if(bin_size >= (end_time - start_time + 1)) {
+    results.push_back(start_time);
+    results.push_back(end_time);
+    PRINTLOG("Cluster: " << root_node << ", Start: " << start_time << ", End: " << end_time);
+    return;
+  }
+  if(root_node < npoints) {
+    if(start_time < start_t) {
+      start_time = start_t;
+    }
+    if(end_time > end_t) {
+      end_time = end_t;
+    }
+    results.push_back(start_time);
+    results.push_back(end_time);
+    PRINTLOG("Cluster-Leaf: " << root_node << ", Start: " << start_time << ", End: " << end_time);
+    return;
+  }
+  // this is a compound node
+  int left_node_index = changeMerge(merge[c_node], npoints);
+  int right_node_index = changeMerge(merge[c_node+npoints-1], npoints);
+  if(end_events[left_node_index] > start_events[right_node_index]) {
+    std::swap(left_node_index, right_node_index);
+  }
+  findClusters(start_t, end_t, bin_size, left_node_index - npoints, results);
+  findClusters(start_t, end_t, bin_size, right_node_index - npoints, results);
+}
+
 vector<double> EventAgglomerateClustering::binnedRangeQuery(int64_t time_begin, int64_t time_end, uint64_t bins) {
-    vector<double> results(bins);
-    uint64_t bin_size(getAGCBinSize(time_begin, time_end, bins));
-    PRINTLOG("Got AGC binned range query");
+  vector<double> results(bins);
+  uint64_t bin_size(getAGCBinSize(time_begin, time_end, bins));
+  PRINTLOG("Got AGC binned range query");
 
-    int* labels = new int[npoints];
+  int* labels = new int[npoints];
+  
+  // cutree_cdist(npoints, merge, height, (double)bin_size, labels);
+  // int s_begin = searchEvent(time_begin, 0);
+  // int s_end = searchEvent(time_end, s_begin);
+
+  // vector<double> data_short_list;
+  // data_short_list.push_back(data[s_begin*2]);
+  // for(int i = s_begin+1; i <= s_end; i++) {
+  //   if(labels[i] == labels[i-1]) continue;
+  //   data_short_list.push_back(data[((i-1)*2)+1]);
+  //   data_short_list.push_back(data[(i*2)]);
+  // }
+  // data_short_list.push_back(data[(s_end*2)+1]);
+
+  vector<int64_t> data_short_list;
+  findClusters(time_begin, time_end, (int64_t)bin_size, npoints-2, data_short_list);
+
+  for(long unsigned int i = 0; i < data_short_list.size(); i+=2) {
+    int64_t start_time = data_short_list[i];
+    int64_t end_time = data_short_list[i+1]; 
     
-    cutree_cdist(npoints, merge, height, (double)bin_size, labels);
-    int s_begin = searchEvent(time_begin, 0);
-    int s_end = searchEvent(time_end, s_begin);
-
-    // for(int i = s_begin; i <= s_end; i++) {
-    //   cout << fixed << setprecision(0) << "i: " << i << " " << data[i*2] << " " << data[(i*2)+1] << " " << labels[i] << endl;
-    // }
-
-    vector<double> data_short_list;
-    data_short_list.push_back(data[s_begin*2]);
-    for(int i = s_begin+1; i <= s_end; i++) {
-      if(labels[i] == labels[i-1]) continue;
-      data_short_list.push_back(data[((i-1)*2)+1]);
-      data_short_list.push_back(data[(i*2)]);
-    }
-    data_short_list.push_back(data[(s_end*2)+1]);
-
-    for(long unsigned int i = 0; i < data_short_list.size(); i+=2) {
-      int64_t start_time = data_short_list[i];
-      int64_t end_time = data_short_list[i+1]; 
-      
-      if(end_time < time_begin || start_time > time_end) continue;
+    if(end_time < time_begin || start_time > time_end) continue;
 
 
-      if(start_time < time_begin) start_time = time_begin;
-      if(start_time > time_end) continue;
-      if(end_time < time_begin) continue;
-      if(end_time > time_end) end_time = time_end;
-      int64_t startingBin = getAGCBinNumber(time_begin, time_end, bins, start_time);
-      int64_t endingBin = getAGCBinNumber(time_begin, time_end, bins, end_time);
-      if(startingBin < 0 || endingBin < 0) continue;
+    if(start_time < time_begin) start_time = time_begin;
+    if(start_time > time_end) continue;
+    if(end_time < time_begin) continue;
+    if(end_time > time_end) end_time = time_end;
+    int64_t startingBin = getAGCBinNumber(time_begin, time_end, bins, start_time);
+    int64_t endingBin = getAGCBinNumber(time_begin, time_end, bins, end_time);
+    if(startingBin < 0 || endingBin < 0) continue;
 
-      for(int64_t bin_it = startingBin+1;
-      bin_it < endingBin && bin_it < (int64_t)bins && results[bin_it] < 0.5; 
-      bin_it++)
-      results[bin_it] = 1.0;
-      
-      if(startingBin < (int64_t)bins && results[startingBin] < 0.5) 
-        results[startingBin] = (start_time % bin_size)?0.5:1.0;
-      if(endingBin < (int64_t)bins && results[endingBin] < 0.5)
-        results[endingBin] = (end_time % bin_size)?0.5:1.0;
-    }
+    for(int64_t bin_it = startingBin+1;
+    bin_it < endingBin && bin_it < (int64_t)bins && results[bin_it] < 0.5; 
+    bin_it++)
+    results[bin_it] = 1.0;
     
+    if(startingBin < (int64_t)bins && results[startingBin] < 0.5) 
+      results[startingBin] = (start_time % bin_size)?0.5:1.0;
+    if(endingBin < (int64_t)bins && results[endingBin] < 0.5)
+      results[endingBin] = (end_time % bin_size)?0.5:1.0;
+  }
+  
+  data_short_list.clear();
   delete[] labels;
   return results;
 }
@@ -174,7 +237,7 @@ LocDict AgglomerateClusters::binnedRangeQuery(int64_t time_begin,
 //     EventAgglomerateClustering agglomerate_clustering;
 
 //     string input_file_path = "/mnt/c/Users/sayef/IdeaProjects/traveler-integrated/data_handler/cgal_libs/cgal_server/location_data/";
-//     fstream input_file(input_file_path + "14.location");
+//     fstream input_file(input_file_path + "9.location");
 //     if (!input_file.is_open()) {
 //         PRINTLOG("Failed to open input file");
 //         return -1;
@@ -188,7 +251,7 @@ LocDict AgglomerateClusters::binnedRangeQuery(int64_t time_begin,
 //     PRINTLOG("Data inserted into the cluster");
 
 //     agglomerate_clustering.buildAggCluster();
-//     vector<double> results = agglomerate_clustering.binnedRangeQuery(1074655386, 1246477086, 20);
+//     vector<double> results = agglomerate_clustering.binnedRangeQuery(1074655386, 1246477086, 100);
 //     PRINTLOG("Results: ");
 //     for (const auto& result : results) {
 //         cout << setprecision(1) << result << " ";
--- data_handler/cgal_libs/cgal_server/agglomerate_clustering.h
+++ data_handler/cgal_libs/cgal_server/agglomerate_clustering.h
@@ -34,6 +34,14 @@ inline int getAGCBinNumber(int64_t time_begin, int64_t time_end, uint64_t bins, 
   return (int)floor((double)(ctime - time_begin) / (double)bin_size);
 }
 
+inline int changeMerge(int i, int N){
+  if (i < 0) {
+    return (i+1) * -1;
+  } else {
+    return (i - 1) + N;
+  }
+}
+
 // this is a one dimensional agglomerate clustering for event sequences.
 // Start and end events will be stored in a vector, even number index will hold the start event and odd number index will hold the end event.
 class EventAgglomerateClustering {
@@ -42,9 +50,12 @@ class EventAgglomerateClustering {
     int* merge = NULL;
     double* height = NULL;
     int* node_size = NULL;
+    double* start_events = NULL;
+    double* end_events = NULL;
     int npoints = 0;
 
     int searchEvent(int64_t tb, int begin_index);
+    void findClusters(int64_t start_t, int64_t end_t, int64_t bin_size, int c_node, std::vector<int64_t> &results);
 
   public:
     EventAgglomerateClustering() {
@@ -57,6 +68,8 @@ class EventAgglomerateClustering {
       delete[] merge;
       delete[] height;
       delete[] node_size;
+      delete[] start_events;
+      delete[] end_events;
     }
 
     void insertDataIntoTree(double start_time, double end_time);
